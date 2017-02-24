72d2defddde3c629a4763a28ed277524f3c93265
zhuruiyu <harvey_zry@icloud.com>
2017-02-24 18:20:30 +0800

    切图

---
+++ node/arraySort.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/2/23 0023.
+ */
+
+
+exports.sortNumber = function(){
+    return arr.sort(function(a,b){
+        return a-b;
+    });
+};
---
+++ node/text.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/2/23 0023.
+ */
+
+var a=require('./arraySort');
+
+var arr2 = a.sortNumber([1,5,3,8,11]);
+
+console.log(arr2);
